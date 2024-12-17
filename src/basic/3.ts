d4b3328520f1559c196602426dfcb261c5f10f63
Kateryna Tsyhaniuk <kyzkakatryska@gmail.com>
2024-12-17 06:57:50 +0200

    start hw ts 1-3task

---
+++ src/basic/3.ts
@@ -0,0 +1,6 @@
+export let element: "ten" | 10;
+
+type EnableOrDisable = "enable" | "disable";
+export let action: EnableOrDisable;
+action = "enable";
+action = "disable";
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { age, callback, empty, toggle, username } from "./basic/1";
+import { person } from "./basic/2";
+import { action, element } from "./basic/3";
+import "./style.css";
+
+console.log(age);
+console.log(username);
+console.log(toggle);
+console.log(empty);
+console.log(callback);
+console.log(person);
+console.log(element);
+console.log(action);
